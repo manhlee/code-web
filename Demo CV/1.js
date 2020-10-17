63dc7ffa350bda6ab864b2a9d646a8be9aaeb2b8
manhlee <manhdzzd@gmail.com>
2020-10-17 21:14:21 +0700

    add cv

---
+++ Demo CV/1.js
@@ -0,0 +1,27 @@
+﻿$(document).ready(function(){
+	var body_width=$(window).width();
+	if(body_width<989)
+	{
+		$('.btn-menu').removeClass('show-btn-menu');
+	}else {
+		$('.btn-menu').addClass('show-btn-menu');
+	}
+	$(window).resize(function(){
+		var body_width=$(window).width();
+	if(body_width<989)
+	{
+		$('.btn-menu').removeClass('show-btn-menu');
+	}else {
+		$('.btn-menu').addClass('show-btn-menu');
+	}
+	});
+});
+
+$(document).ready(function(){
+	$('.btn-menu').click(function(){
+		$('.layer-right').addClass('show-info');
+	});
+	$('.layer-right').click(function(){
+		$('.layer-right').removeClass('show-info');
+	});
+});
